0e083992738f453f349462ac4e0dfc6d2dae2b85
Jakob Sachs <jakobsachs1999@gmail.com>
2022-09-06 13:29:45 +0200

    Rewrote actions for better File handling

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Monad
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BS
 import System.Console.Readline
@@ -7,29 +8,39 @@ import System.Directory
 import System.Environment
 import System.Exit
 import System.IO
+import System.Posix
 import ToDoItem
 
-usage = putStrLn "Usage: haskToDo [help/show/new/delete/...] ..."
+usage = putStrLn "Usage: haskToDo [help/list/new/done/prune/...]"
 
--- show <list name>
--- new
--- delete
+--
+-- utility functions
+--
 
-readToDoList :: String -> IO (Maybe ToDoList)
-readToDoList listName = do
-  json <- BS.readFile (listName <> ".json")
-  return (decode json)
+getToDoList :: String -> IO ToDoList
+getToDoList listName = do
+  usrHome <- getUserHome
+  let fileName = usrHome <> "/.haskToDo/" <> listName <> ".json"
+  doesFileExist <- fileExist fileName
+  if doesFileExist
+    then do
+      json <- BS.readFile fileName
+      let maybeLst = decode json
+      case maybeLst of
+        Just lst -> return lst
+        Nothing -> putStrLn "Failure to parse json" >> exitFailure >> return (ToDoList {listTitle = listName, items = []})
+    else return (ToDoList {listTitle = listName, items = []})
 
 saveToDoList :: ToDoList -> IO ()
-saveToDoList list = BS.writeFile (listTitle list <> ".json") (encode list)
+saveToDoList list = do
+  usrHome <- getUserHome
+  let fileName = usrHome <> "/.haskToDo/" <> (listTitle list) <> ".json"
+  BS.writeFile fileName (encode list)
 
 showToDoList :: String -> IO ()
 showToDoList listName = do
-  decodedList <- readToDoList listName
-  case decodedList of
-    Just lst -> do
-      putStrLn $ unlines . map renderItem $ items lst
-    Nothing -> putStrLn "Error parsing list" >> exitFailure
+  lst <- getToDoList listName
+  putStrLn $ unlines . map renderItem $ items lst
 
 getUserInput :: String -> IO String
 getUserInput query = do
@@ -39,46 +50,87 @@ getUserInput query = do
     Just [] -> putStrLn (query <> " cant be empty!") >> getUserInput query
     Just input -> return input
 
+getConfirmation :: IO Bool
+getConfirmation = do
+  mybResp <- readline "Please confirm (y/N)> "
+  case mybResp of
+    Just "y" -> return True
+    Nothing -> return False
+    Just _ -> return False
+
+getUserHome :: IO String
+getUserHome = homeDirectory <$> (getUserEntryForID =<< getRealUserID)
+
+--
 -- main
+--
 
 main :: IO ()
 main = do
+  usr_home <- getUserHome
+  createDirectoryIfMissing False (usr_home <> "/.haskToDo")
+
   -- parse arguments
   args <- getArgs
   case args of
     ["help"] -> usage
-    -- list items
-    ["list"] -> do
-      fileExists <- doesFileExist "main.json"
-      if fileExists
-        then showToDoList "main"
-        else putStrLn "Nothing to-do!"
-
+    ["list"] -> showToDoList "main"
+    --
     -- add new item
+    --
     ["new"] -> do
       titel <- getUserInput "Titel"
       body <- getUserInput "Body"
       let item = ToDoItem {itemTitle = titel, status = Pending, body = body}
       -- parse list
+      lst <- getToDoList "main"
+      -- add new item to list and save it
+      let new_lst =
+            ToDoList
+              { listTitle = (listTitle lst),
+                items = ((items lst) ++ [item])
+              }
+      saveToDoList new_lst
+
+    --
+    -- change items status
+    --
+    ["done"] -> do
+      titel <- getUserInput "Titel"
       fileExists <- doesFileExist "main.json"
       if fileExists
         then do
-          my_lst <- readToDoList "main"
+          lst <- getToDoList "main"
 
           -- add new item to list and save it
-          case my_lst of
-            Just lst -> do
-              let new_lst =
-                    ToDoList
-                      { listTitle = (listTitle lst),
-                        items = ((items lst) ++ [item])
-                      }
+          let orig_items = items lst
+          let old_item = head $ filter (\x -> itemTitle x == titel) orig_items
+          let new_item = old_item {status = Done}
+          let new_lst = lst {items = ((filter (\x -> itemTitle x /= titel) orig_items) <> [new_item])}
+          saveToDoList new_lst
+        else do
+          -- just save to json file
+          putStrLn "No current tasks"
+    --
+    -- remove items with Done status
+    --
+    ["prune"] -> do
+      -- read in list, if file exists
+      lst <- getToDoList "main"
+      let amountOfDoneItems = length (filter (\x -> status x == Done) $ items lst)
+      case amountOfDoneItems of
+        0 -> putStrLn "No items to cleanup!"
+        _ -> do
+          putStrLn $ "Are you sure you want to delete " ++ show amountOfDoneItems ++ " item(s)?"
+          confirm <- getConfirmation
+          if confirm
+            then do
+              -- filer out done items
+              let new_lst = lst {items = (filter (\x -> status x /= Done) $ items lst)}
               saveToDoList new_lst
-            Nothing -> putStrLn "failed to parse json" >> exitFailure
-        else -- just save to json file
-        do
-          let lst = ToDoList {listTitle = "main", items = [item]}
-          saveToDoList lst
+              putStrLn "Cleaned items"
+            else do
+              putStrLn "Stopping"
 
     -- no input case
     _ -> putStrLn "Error parsing input (try 'help')" >> exitFailure
